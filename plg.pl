54da1cef737cc799c9f4432917d1ccd9b61a21f8
Carsen Ball <ballcarsen@gmail.com>
2019-03-04 11:38:23 -0700

    almost

--- plg.pl
+++ plg.pl
@@ -4,9 +4,10 @@ adjacent(X,Y) :- X =:= Y-1.
 offices([office(_,1), office(_,2), office(_,3), office(_,4), office(_,5)]).
 
 layout(X) :- offices(X),
-    member(office(jack, A),X),
-    member(office(laura,B),X), B\=1,
+    member(office(jim, D), X), D\=1, D\=5,
+    member(office(jack, A),X), \adjacent(A,D),
+    member(office(laura,B),X), B\=1, \adjacent(D,B),
     member(office(sally,C), X), C > B,
-    member(office(jim, D), X), D\=1, D\=5, \adjacent(D,A), \adjacent(D,B),
     member(office(hunter, E),X), E\=5.
+
 
