aa8dc61a379c43219ad5494dd0f41ebf5b994908
carsen.ball <n55p212@csci305.cs.montana.edu>
2019-03-04 14:21:06 -0700

    finished

--- plg.pl
+++ plg.pl
@@ -6,8 +6,8 @@ offices([office(_,1), office(_,2), office(_,3), office(_,4), office(_,5)]).
 layout(X) :- offices(X),
     member(office(jack,B),X),
     member(office(laura,C),X), C\=1,
-    member(office(sally,D),X),
-    member(office(jim,A),X), A\=1, A\=5, \adjacent(A,B),\adjacent(A,C),
+    member(office(sally,D),X), D > C,
+    member(office(jim,A),X), A\=1, A\=5, \+ adjacent(A,B), \+ adjacent(A, C),
     member(office(hunter,E),X), E\=5.
 
 
